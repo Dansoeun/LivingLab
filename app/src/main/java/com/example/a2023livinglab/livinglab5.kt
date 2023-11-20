f5f811ec08016f31e3377138536b4a2a0c26b33f
김나현 <rlaskgus0801@gmail.com>
2023-11-20 23:08:21 +0900

    ㅎㅎ

---
+++ app/src/main/java/com/example/a2023livinglab/livinglab5.kt
@@ -0,0 +1,11 @@
+package com.example.a2023livinglab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class livinglab5 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_livinglab5)
+    }
+}
